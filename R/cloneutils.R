36e0ee24610ecf55b01daab199e399198973ad84
Kieran Campbell <kieranrcampbell@gmail.com>
2018-04-30 12:47:20 -0700

    Initial commit

---
+++ R/cloneutils.R
@@ -0,0 +1,44 @@
+
+#' Compute clonealign fits across a range of highly variable genes
+#'
+#' @export
+compute_cal_fits <- function(hvg_props,
+                             sce,
+                             cnv_data) {
+  rvs <- rowVars(as.matrix(logcounts(sce)))
+  ca_reps <- lapply(hvg_props, function(rvq) {
+    genes_to_sample <- rvs > quantile(rvs, rvq)
+    em <- clonealign(sce[genes_to_sample,], cnv_data[genes_to_sample,], rel_tol_em = 1e-8)
+    em
+  })
+  ca_reps
+}
+
+#' Construct a precision recall curve using the output of compute_cal_fits
+#'
+#' @export
+construct_pr_df <- function(cal_fits, base_fit, hvg_props) {
+  calculate_precision_recall <- function(base_fit, new_fit, hvgp) {
+    mat <- table(new_fit$clone, base_fit$clone)
+    precision <- diag(mat) / rowSums(mat)
+    recall <- (diag(mat) / colSums(mat))
+    data_frame(precision = precision, recall = recall, clone = names(precision), hvg_prop = hvgp)
+  }
+
+  dfs <- lapply(seq_along(hvg_props), function(i) calculate_precision_recall(base_fit, cal_fits[[i]], hvg_props[i]))
+  df_pr <- bind_rows(dfs) %>%
+    gather(measure, value, -clone, -hvg_prop)
+  df_pr
+}
+
+#' Plot the precision recall curve output by construct_pr_df
+#'
+#' @export
+plot_pr <- function(df_pr) {
+  ggplot(df_pr, aes(x = hvg_prop, y = value, color = clone, linetype = measure, shape = measure)) +
+    geom_point() +
+    geom_line() +
+    ggsci::scale_color_d3() +
+    labs(x = "Quantile highly variable genes", y = "Measure value")
+}
+
